c303d40738ab818fb682653386ff078ec1ae6907
David King <dave@davbo.org>
2012-09-26 17:08:17 +0100

    Added a Moxie config file for configuring url routes.

---
+++ js/moxie.conf.js
@@ -0,0 +1,15 @@
+define([], function() {
+    var MoxieConf = {
+        endpoint: 'http://localhost:5000',
+        paths: {
+            places_search: '/places/search'
+        },
+        urlFor: function(api_method) {
+            return this.endpoint + this.paths[api_method];
+        },
+        pathFor: function(api_method) {
+            return this.paths[api_method];
+        }
+    };
+    return MoxieConf;
+});
