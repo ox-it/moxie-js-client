4ea91ed0c68930c13ad3b200089e2b5217b5690e
David King <dave@davbo.org>
2012-09-26 17:11:25 +0100

    Renamed the PlacesRouter

--- js/places/router.js
+++ js/places/router.js
@@ -1,6 +1,6 @@
 define(["jquery","backbone","places/models/POIModel", "places/views/SearchView", "places/collections/POICollection"], function($, Backbone, POI, SearchView, POIs){
 
-    var Router = Backbone.Router.extend({
+    var PlacesRouter = Backbone.Router.extend({
 
         // All of your Backbone Routes (add more)
         routes: {
@@ -10,26 +10,22 @@ define(["jquery","backbone","places/models/POIModel", "places/views/SearchView",
         },
 
         search: function(params) {
-            console.log("Search called!");
             // Instantiating the mainView instance
             results = new POIs();
             searchView = new SearchView({
 
-                // TODO: Run the default search here?
                 // Declares the View's collection instance property
                 collection: results,
-                params: params,
-
+                params: params
             });
-            console.log(searchView);
 
             // Renders all of the User Model's to the page
             searchView.render();
-
         }
+
     });
 
     // Returns the Router class
-    return Router;
+    return PlacesRouter;
 
 });
