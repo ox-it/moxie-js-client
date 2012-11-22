97093bc6f356b1737e2c7a8e0510f23ed82b08d5
David King <dave@davbo.org>
2012-11-22 18:13:14 +0000

    Removed duplicate Handlebars definition
    spaces not tabs!

--- js/main.js
+++ js/main.js
@@ -8,15 +8,14 @@ require.config({
         "jquery": "libs/jquery",
         "underscore": "libs/underscore",
         "backbone": "libs/backbone",
-		"Handlebars": "libs/Handlebars",
-		"handlebars": "libs/Handlebars",
-		"hbs": "libs/hbs",
-		"json2": "libs/json2",
-		"i18nprecompile": "libs/i18nprecompile",
+        "handlebars": "libs/Handlebars",
+        "hbs": "libs/hbs",
+        "json2": "libs/json2",
+        "i18nprecompile": "libs/i18nprecompile",
         "leaflet": "libs/leaflet",
         "time_domain": "libs/time_domain",
         "backbone.queryparams": "libs/backbone.queryparams",
-		"backbone.hal": "libs/backbone.hal",
+        "backbone.hal": "libs/backbone.hal",
     },
 
     // Sets the configuration for your third party scripts that are not AMD compatible
@@ -37,37 +36,37 @@ require.config({
         "time_domain": {
             "exports": "TimeDomain",
         },
-		"handlebars": {
-			"exports": "Handlebars",
-		},
-		"json2": {
-			"exports": "JSON",
-		}
+        "handlebars": {
+            "exports": "Handlebars",
+        },
+        "json2": {
+            "exports": "JSON",
+        }
     },
-	
-	hbs: {
-		templateExtension: 'handlebars',
-		disableI18n: true,
-		partialPath: '/handlebars/'
-	},
+
+    hbs: {
+        templateExtension: 'handlebars',
+        disableI18n: true,
+        partialPath: '/handlebars/',
+    },
 });
 
 require(['modernizr','jquery','backbone','places/router', 'today/router', 'courses/router'],
-	function(Modernizr, $, Backbone, PlacesRouter, TodayRouter, CoursesRouter) {
+    function(Modernizr, $, Backbone, PlacesRouter, TodayRouter, CoursesRouter) {
 
-    placesRouter = new PlacesRouter();
-    todayRouter = new TodayRouter();
-	coursesRouter = new CoursesRouter();
+        placesRouter = new PlacesRouter();
+        todayRouter = new TodayRouter();
+        coursesRouter = new CoursesRouter();
 
-    // Extend the View class to include a navigation method goTo
-    Backbone.View.prototype.goTo = function (loc, options) {
-        placesRouter.navigate(loc, options);	// this should be on Backbone.Router.routes I guess??
-    };
-    Backbone.history.start();
-    $('#home a').click(function(ev) {
-        ev.preventDefault();
-        window.history.back();
-        return false;
+        // Extend the View class to include a navigation method goTo
+        Backbone.View.prototype.goTo = function (loc, options) {
+            placesRouter.navigate(loc, options);	// this should be on Backbone.Router.routes I guess??
+        };
+        Backbone.history.start();
+        $('#home a').click(function(ev) {
+            ev.preventDefault();
+            window.history.back();
+            return false;
+        });
+
     });
-
-});
