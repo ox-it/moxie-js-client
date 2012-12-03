73f5975f193cc3b3869f9b9af856b60984a450e2
Martin Filliau <martin@filliau.com>
2012-12-03 14:12:04 +0000

    Courses / added date formatting.

---
+++ js/courses/helpers.js
@@ -0,0 +1,16 @@
+define('template/helpers/dateFormat', ["handlebars"], function(Handlebars) {
+    function dateFormat(string) {
+        if(!string) {
+            return "";
+        }
+        try {
+            date = new Date(string);
+            return date.getDay() +  "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+        } catch (err) {
+            console.log(err);
+            return "";
+        }
+    }
+    Handlebars.registerHelper('dateFormat', dateFormat);
+    return dateFormat;
+});
