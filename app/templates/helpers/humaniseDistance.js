667b747928a181e0ac80395265ecc520bc3eb400
Tim Fernando <tim.fernando@it.ox.ac.uk>
2012-12-13 19:34:34 +0000

    removed superfluous space between metres and distance

--- app/templates/helpers/humaniseDistance.js
+++ app/templates/helpers/humaniseDistance.js
@@ -2,9 +2,9 @@ define(["handlebars", "time_domain"], function(Handlebars, TimeDomain) {
     function humaniseDistance(distance) {
         distance = parseFloat(distance);
         if(distance >= 1.0) {
-            return Math.round(distance*10)/10 + " km";
+            return Math.round(distance*10)/10 + "km";
         } else {
-            return Math.round(distance*1000) + " m";
+            return Math.round(distance*1000) + "m";
         }
     }
     Handlebars.registerHelper('humaniseDistance', humaniseDistance);
