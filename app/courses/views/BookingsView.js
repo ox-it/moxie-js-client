ca2839be67f870157a09f4a09cc29276f2b1afe9
David King <dave@davbo.org>
2013-01-10 15:24:53 +0000

    Course withdrawal handler, calls up to the API with a DELETE method.

--- app/courses/views/BookingsView.js
+++ app/courses/views/BookingsView.js
@@ -6,6 +6,11 @@ define(['jquery', 'backbone', 'underscore', 'hbs!courses/templates/bookings', 'l
             _.bindAll(this);
         },
 
+        events: {
+            'click .updateStatus': "updateStatus",
+            'click .withdrawBooking': "withdrawBooking"
+        },
+
         render: function() {
             $.ajax({
                 url: MoxieConf.urlFor('courses_bookings'),
@@ -15,6 +20,28 @@ define(['jquery', 'backbone', 'underscore', 'hbs!courses/templates/bookings', 'l
                 }
             }).success(this.renderCoursesList);
             return this;
+        },
+
+        updateStatus: function(e) {
+            e.preventDefault();
+            $(e.target).parent().siblings().show();
+            return false;
+        },
+
+        withdrawBooking: function(e) {
+            // Makes a DELETE ajax call to the API to remove a booking
+            e.preventDefault();
+            var presentation_id = $(e.target).parent().parent().data('id');
+            var url = MoxieConf.urlFor('presentation_id') + presentation_id + '/booking';
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                dataType: 'json',
+                xhrFields: {
+                    withCredentials: true
+                }
+            }).success(this.render);
+            return false;
         },
 
         renderCoursesList: function(data) {
