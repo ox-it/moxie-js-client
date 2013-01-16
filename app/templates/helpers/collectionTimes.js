c9f6312dd276a0818907eb5c70b240111e05efd4
Martin Filliau <martin@filliau.com>
2013-01-16 11:51:14 +0000

    Places / added indicator for postboxes "collected" or "not yet collected" (TRELLO#110).

---
+++ app/templates/helpers/collectionTimes.js
@@ -0,0 +1,22 @@
+define(["handlebars"], function(Handlebars) {
+    function collectionTimes(string) {
+        // Function that takes a collection times string and returns a 'true' if
+        // it has been collected today else 'false'
+        if(!string) {
+            return "";
+        }
+        try {
+            result = TimeDomain.evaluateNextTime(string);
+        } catch (err) {
+            console.log(err);
+            return "";
+        }
+        if(result.value === true) {
+            return "<span class='label-place-open'>collected</span>";
+        } else {
+            return "<span class='label-place-closed'>not yet collected</span>";
+        }
+    }
+    Handlebars.registerHelper('collectionTimes', collectionTimes);
+    return collectionTimes;
+});
