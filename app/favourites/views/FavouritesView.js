e6e64f1dd03be59384f8034032043de7b8068a0a
David King <dave@davbo.org>
2013-02-07 17:32:55 +0000

    Simple UI for browsing favourites
    The obvious next step is some way to manage/remove favourites from here.

--- app/favourites/views/FavouritesView.js
+++ app/favourites/views/FavouritesView.js
@@ -1,8 +1,15 @@
-define(['jquery', 'backbone', 'underscore', 'favourites/models/Favourite', 'favourites/collections/Favourites'],
-    function($, Backbone, _, Favourite, Favourites) {
+define(['jquery', 'backbone', 'underscore', 'favourites/collections/Favourites', 'hbs!favourites/templates/favourites'],
+    function($, Backbone, _, Favourites, favouritesTemplate) {
         var FavouritesView = Backbone.View.extend({
             initialize: function() {
                 _.bindAll(this);
+                this.favourites = new Favourites();
+                this.favourites.fetch();
+                this.favourites.on("remove add", this.render, this);
+            },
+            render: function() {
+                Backbone.trigger('domchange:title', "Favourites");
+                this.$el.html(favouritesTemplate({favourites: this.favourites.toJSON()}));
             }
         });
         return FavouritesView;
