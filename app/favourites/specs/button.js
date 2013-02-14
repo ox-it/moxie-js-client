d9b651151c45fe788dc185269f0490b910f5fcbb
David King <dave@davbo.org>
2013-02-14 16:51:21 +0000

    Added test for ordering of querystrings in favourites

--- app/favourites/specs/button.js
+++ app/favourites/specs/button.js
@@ -1,11 +1,12 @@
-define(["jquery", "backbone", "jasmine", "favourites/views/FavouriteButtonView", "moxie.conf"], function($, Backbone, jasmine, FavouriteButtonView, conf) {
+define(["jquery", "backbone", "jasmine", "favourites/views/FavouriteButtonView", "moxie.conf", 'backbone.queryparams'], function($, Backbone, jasmine, FavouriteButtonView, conf) {
+    Backbone.history.start();
 
     describe("Behaviour of clicking the favourites button", function() {
         var el = $('<a></a>');
         var button = new FavouriteButtonView({el: el});
 
         afterEach(function() {
-            button.favourites.forEach(function(model) { model.destroy(); });
+            button.favourites.remove(button.favourites.models);
         });
 
         it("Should set a class when clicked", function() {
@@ -35,15 +36,16 @@ define(["jquery", "backbone", "jasmine", "favourites/views/FavouriteButtonView",
         });
     });
     describe("Behaviour of clicking the favourites button", function() {
-        var el = $('<a></a>');
-        var button = new FavouriteButtonView({el: el});
 
+        var button;
         var pageTitle;
         beforeEach(function() {
+            var el = $('<a></a>');
+            button = new FavouriteButtonView({el: el});
+            button.favourites.remove(button.favourites.models);
             pageTitle = document.title;
         });
         afterEach(function() {
-            button.favourites.forEach(function(model) { model.destroy(); });
             document.title = pageTitle;
         });
         it("Should set a sensible title for the favourite", function() {
@@ -52,9 +54,18 @@ define(["jquery", "backbone", "jasmine", "favourites/views/FavouriteButtonView",
             expect(button.favourites.first().get('title')).toBe("Kaboom!");
         });
         it("Should save the current hash with the favourite", function() {
-            Backbone.history.fragment = 'foobar';
+            Backbone.history.navigate('foobar', {trigger:false});
             button.$el.click();
             expect(button.favourites.first().get('fragment')).toBe("foobar");
+            // leave this undefined as we don't start backbone.history for our tests
+            Backbone.history.fragment = undefined;
+        });
+        it("Shouldn't save two favourites with different query string ordering", function() {
+            Backbone.history.navigate('foo?bar=1&baz=2', {trigger:false});
+            button.$el.click();
+            Backbone.history.navigate('foo?baz=2&bar=1', {trigger:false});
+            button.$el.click();
+            expect(button.favourites.toJSON().length).toBe(0);
             // leave this undefined as we don't start backbone.history for our tests
             Backbone.history.fragment = undefined;
         });
