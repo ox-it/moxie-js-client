5b71de2fdc8f9b21462028cc64041c4e4e6c3226
David King <dave@davbo.org>
2013-02-14 16:50:45 +0000

    Destroy favourites as they are removed from the collection

--- app/favourites/models/Favourite.js
+++ app/favourites/models/Favourite.js
@@ -2,6 +2,11 @@ define(["backbone"],
 
     function(Backbone) {
         var Favourite = Backbone.Model.extend({
+            initialize: function() {
+                this.bind('remove', function() {
+                    this.destroy();
+                });
+            }
         });
         return Favourite;
     }
