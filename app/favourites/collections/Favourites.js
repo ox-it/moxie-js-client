b74f490bd5a8353a0de90012a9a2595d39a2f3b2
David King <dave@davbo.org>
2013-02-14 16:52:20 +0000

    getCurrentPage compares queryparams for equality
    This uses underscores handy isEqual function

--- app/favourites/collections/Favourites.js
+++ app/favourites/collections/Favourites.js
@@ -1,15 +1,14 @@
-define(["backbone", "favourites/models/Favourite", "localstorage"],
+define(["backbone", "underscore", "favourites/models/Favourite", "localstorage"],
 
-    function(Backbone, Favourite) {
+    function(Backbone, _, Favourite) {
         var Favourites = Backbone.Collection.extend({
             model: Favourite,
             localStorage: new Backbone.LocalStorage("favourites"), // Unique name within your app.
 
-            getCurrentPage: function(fragment) {
-                if (fragment === undefined) {
-                    fragment = Backbone.history.fragment;
-                }
-                return this.find(function(model) { return (model.get('fragment') == fragment); });
+            getCurrentPage: function() {
+                var fragment = Backbone.history.getFragment(undefined, undefined, true);
+                var params = Backbone.history.getQueryParameters();
+                return this.find(function(model) { return (model.get('fragment') == fragment && _.isEqual(model.get('params'), params)); });
             }
         });
         return Favourites;
