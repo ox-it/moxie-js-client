e2c792b7d191df35cf2096cf1252d886d963e2ef
Martin Filliau <martin@filliau.com>
2013-02-18 16:01:26 +0000

    Refactored a bit moxie.position.js
    Extracted string put into a variable.

--- app/moxie.position.js
+++ app/moxie.position.js
@@ -1,29 +1,30 @@
 define(["underscore", "backbone", "moxie.conf"], function(_, Backbone, conf){
+    var EVENT_POSITION_UPDATED = 'position:updated';
     var userPosition = {
         count: 0,
         follow: function(cb) {
             if (this.count===0) {
                 this.startWatching();
             }
-            this.on('position:updated', cb);
+            this.on(EVENT_POSITION_UPDATED, cb);
             if (this.latest) {
                 // New subscribers should get the latest location fix
-                this.trigger('position:updated', this.latest);
+                this.trigger(EVENT_POSITION_UPDATED, this.latest);
             }
             this.count++;
         },
         unfollow: function(cb) {
-            this.off('position:updated', cb);
+            this.off(EVENT_POSITION_UPDATED, cb);
             this.count--;
         },
         locationSuccess: function(position) {
-            this.trigger('position:updated', position);
+            this.trigger(EVENT_POSITION_UPDATED, position);
             this.latest = position;
         },
         locationError: function(e) {
             if (!this.latest) {
                 // We have no good position data so update to the default location
-                this.trigger('position:updated', conf.defaultLocation);
+                this.trigger(EVENT_POSITION_UPDATED, conf.defaultLocation);
                 this.latest = conf.defaultLocation;
             }
         },
@@ -31,7 +32,7 @@ define(["underscore", "backbone", "moxie.conf"], function(_, Backbone, conf){
             // Ask for immediate position then watch with a big timeout / max age
             navigator.geolocation.getCurrentPosition(this.locationSuccess, this.locationError);
             this.watchID = navigator.geolocation.watchPosition(this.locationSuccess, this.locationError,
-            {maximumAge: 120000, timeout:25000}); // This is useful for debugging problem with geolocation
+                {maximumAge: 120000, timeout:25000}); // This is useful for debugging problem with geolocation
         }
     };
     _.bindAll(userPosition);
