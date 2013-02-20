09a4c121b3685131f258a8162d6c0fe993a8ffe0
David King <dave@davbo.org>
2013-02-20 16:21:51 +0000

    Refactored infinite scroll view
    Scroll element and window scroll are now passed through
    to initscroll as arguments

--- app/core/views/InfiniteScrollView.js
+++ app/core/views/InfiniteScrollView.js
@@ -2,47 +2,45 @@ define(['jquery', 'backbone', 'underscore'], function($, Backbone, _){
     var InfiniteScrollView = Backbone.View.extend({
         scrollCallbacks: [function(){ console.log("Scrolled!"); }],
         infiniteScrollEnabled: true,
-        windowScroll: true,
 
         // Used to capture once a scroll has occured and on which element
-        divScrolled: false,
+        elScrolled: false,
         windowScrolled: false,
 
-        initScroll: function() {
-            if (this.windowScroll) {
+        handleScroll: function() {
+            if ((this.infiniteScrollEnabled) && (this.windowScrolled || this.elScrolled)) {
+                this.infiniteScrollEnabled = false; // Infinite scroll disabled for duration of handling 1 event
+                var thresholdSurpassed = false;
+                if (this.scrollThreshold) {
+                    if (this.elScrolled) {
+                        thresholdSurpassed = ((this.scrollElement.scrollTop / this.scrollElement.scrollHeight) > this.scrollThreshold);
+                    } else {
+                        thresholdSurpassed = (($(document).scrollTop() / $(document).height()) > this.scrollThreshold);
+                    }
+                }
+                if ((this.scrollThreshold && thresholdSurpassed) || !this.scrollThreshold) {
+                    _.each(this.scrollCallbacks, _.bind(function(cb) { cb.apply(this); }, this));
+                }
+                this.infiniteScrollEnabled = true;
+                this.windowScrolled = false;
+                this.elScrolled = false;
+            }
+        },
+
+        initScroll: function(scrollElement, windowScroll) {
+            if (windowScroll) {
                 $(window).scroll(
                     _.bind(function(){ this.windowScrolled = true; }, this)
                 );
             }
-            if (this.divScroll) {
-                this.$(this.divScroll).scroll(
-                    _.bind(function(){ this.divScrolled = true; }, this)
+            if (scrollElement) {
+                this.scrollElement = scrollElement;
+                $(scrollElement).scroll(
+                    _.bind(function(){ this.elScrolled = true; }, this)
                 );
             }
-            if (this.windowScroll || this.divScroll) {
-                this.scrollInterval = window.setInterval(_.bind(function(){
-                        if ((this.infiniteScrollEnabled) && (this.windowScrolled || this.divScrolled)) {
-                            this.infiniteScrollEnabled = false; // Infinite scroll disabled for duration of handling 1 event
-                            var thresholdSurpassed = false;
-                            if (this.scrollThreshold) {
-                                if (this.divScrolled) {
-                                    _.each(this.$(this.divScroll), _.bind(function(el) {
-                                        if (!thresholdSurpassed) {
-                                            thresholdSurpassed = ((el.scrollTop / el.scrollHeight) > this.scrollThreshold);
-                                        }
-                                    }, this));
-                                } else {
-                                    thresholdSurpassed = (($(document).scrollTop() / $(document).height()) > this.scrollThreshold);
-                                }
-                            }
-                            if ((this.scrollThreshold && thresholdSurpassed) || !this.scrollThreshold) {
-                                _.each(this.scrollCallbacks, _.bind(function(cb) { cb.apply(this); }, this));
-                            }
-                            this.infiniteScrollEnabled = true;
-                            this.windowScrolled = false;
-                            this.divScrolled = false;
-                        }
-                }, this), 250); // limit to 250ms per Mr. Resig's suggestion
+            if (windowScroll || scrollElement) {
+                this.scrollInterval = window.setInterval(_.bind(this.handleScroll, this), 250); // limit to 250ms per Mr. Resig's suggestion
             }
         },
 
