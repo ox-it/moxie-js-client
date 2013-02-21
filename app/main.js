1441d4c3b47ec9cfab81e46e8cdc7218946c4989
Martin Filliau <martin@filliau.com>
2013-02-21 17:07:18 +0000

    Global error handler for JQuery.
    Just does an alert() for now.

--- app/main.js
+++ app/main.js
@@ -9,6 +9,11 @@ require(['modernizr','jquery','backbone', 'router', 'fastclick', 'moxie.conf', '
     // Default to requesting hal+json but fallback to json
     $.ajaxSetup({ headers: { 'Accept': 'application/hal+json;q=1.0, application/json;q=0.9, */*; q=0.01' } });
 
+    // Global error handling for AJAX calls
+    $(document).ajaxError(function(event, jqxhr, settings, exception){
+        alert("Sorry, an error has occurred while connecting to the server.");
+    });
+
     // Add an event listener for sending document title changes
     Backbone.on('domchange:title', function(title) {$(document).attr('title', conf.titlePrefix+title);}, this);
 
