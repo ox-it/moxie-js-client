ox-it/moxie-js-client

commit 4712e48dded5b3a8b6f0bd3bb784dbd072608474

Places views: do not assume anymore that a POI has a lat/lon... (TRELLO#189).

app/places/views/DetailView.js
@@ -70,9 +70,11 @@ define(['jquery', 'backbone', 'underscore', 'leaflet', 'moxie.conf', 'moxie.posi
             var context = {'poi': this.poi, 'rti': rti};
             this.$("#list").html(detailTemplate(context));
             this.$el.scrollTop(0);
-            this.latlng = new L.LatLng(this.poi.get('lat'), this.poi.get('lon'));
-            this.marker = new L.marker(this.latlng, {'title': this.poi.get('name')});
-            this.marker.addTo(this.map);
+            if(this.poi.lat && this.poi.lon) {
+                this.latlng = new L.LatLng(this.poi.get('lat'), this.poi.get('lon'));
+                this.marker = new L.marker(this.latlng, {'title': this.poi.get('name')});
+                this.marker.addTo(this.map);
+            }
             this.updateMap();
             if (rti) {
                 var url = MoxieConf.endpoint + rti.href;

app/places/views/EmbeddedPoiView.js
@@ -20,7 +20,10 @@ define(['jquery', 'backbone', 'underscore', 'leaflet', 'moxie.conf', 'moxie.posi
 
         renderPOI: function(data) {
             this.$el.html(embeddedTemplate(data));
-            this.renderMap(data);
+            // only renders the map if we've lat/lon
+            if(data.lat && data.lon) {
+                this.renderMap(data);
+            }
         },
 
         renderMap: function(data) {

app/places/views/SearchView.js
@@ -73,11 +73,13 @@ define(['jquery', 'backbone', 'underscore', 'leaflet', 'moxie.conf', 'moxie.posi
         },
 
         placePOI: function(poi) {
-            var latlng = new L.LatLng(poi.attributes.lat, poi.attributes.lon);
-            var marker = new L.marker(latlng, {'title': poi.attributes.name});
-            marker.addTo(this.map);
-            this.latlngs.push(latlng);
-            this.markers.push(marker);
+            if(poi.attributes.lat && poi.attributes.lon) {
+                var latlng = new L.LatLng(poi.attributes.lat, poi.attributes.lon);
+                var marker = new L.marker(latlng, {'title': poi.attributes.name});
+                marker.addTo(this.map);
+                this.latlngs.push(latlng);
+                this.markers.push(marker);
+            }
         },
 
         setMapBounds: function() {

commit 8fc4ff6995969082289ae57ebec30988be700c80

Fixed bug introduced in 4712e48 where a marker might not be present anymore (TRELLO#189).

app/places/views/DetailView.js
@@ -20,7 +20,9 @@ define(['jquery', 'backbone', 'underscore', 'leaflet', 'moxie.conf', 'moxie.posi
 
         navigateBack: function(ev) {
             ev.preventDefault();
-            this.map.removeLayer(this.marker);
+            if(this.marker) {
+                this.map.removeLayer(this.marker);
+            }
             this.cb();
             this.onClose();
         },