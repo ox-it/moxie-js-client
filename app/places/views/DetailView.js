e893f75c869ab7b37a18b2d181905299ff3678ae
Martin Filliau <martin@filliau.com>
2013-02-25 10:23:04 +0000

    Clear interval (refreshRTI) on closing a view.

--- app/places/views/DetailView.js
+++ app/places/views/DetailView.js
@@ -83,7 +83,7 @@ define(['jquery', 'backbone', 'underscore', 'leaflet', 'moxie.conf', 'moxie.posi
             this.updateMap();
             if (this.rti) {
                 this.refreshRTI();
-                setInterval(this.refreshRTI, RTI_REFRESH);
+                this.refreshID = setInterval(this.refreshRTI, RTI_REFRESH);
             }
         },
 
@@ -120,6 +120,7 @@ define(['jquery', 'backbone', 'underscore', 'leaflet', 'moxie.conf', 'moxie.posi
         onClose: function() {
             $('#back').hide().off('click');
             userPosition.unfollow(this.handle_geolocation_query);
+            clearInterval(this.refreshID);
         }
 
     });
