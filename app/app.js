96ff1c4880b3fb1cbc30fac573bab40caff751d3
David King <dave@davbo.org>
2013-02-28 17:17:35 +0000

    App object now handles loading Layouts
    If the layout is already loaded then we handle that.

--- app/app.js
+++ app/app.js
@@ -1,75 +1,28 @@
-define(['jquery', 'backbone', 'underscore'], function($, Backbone, _) {
-    function App(){
-        this.pageStack = 0;
-        this.startingPage = function() {
-            return this.pageStack == 1;
-        };
-        this.navigate = function(fragment, options) {
-            options = options ? options : {};
-            if (options.replace !== true) {
-                this.pageStack++;
-            }
-            return Backbone.history.navigate(fragment, options);
-        };
-        var defaultBack = function(ev) {
-            this.pageStack -= 2;
-            ev.preventDefault();
-            window.history.back();
-            return false;
-        };
-        this.showBack = function(cb) {
-            $('#home').hide();
-            var back = $('#back');
-            back.show();
-            var backA = back.find('a');
-            backA.off('click');
-            if (cb) {
-                backA.one('click', cb);
-                backA.one('click', _.bind(function() { this.pageStack -= 2; }, this));
+define(['jquery', 'underscore', 'core/views/MapBrowseLayout'], function($, _, MapBrowseLayout) {
+    var app = {
+
+        currentLayout: null,
+
+        layouts: {'MapBrowseLayout': MapBrowseLayout},
+
+        getLayout: function(name) {
+            if (this.currentLayout && this.currentLayout.name === name) {
+                return this.currentLayout;
             } else {
-                backA.one('click', _.bind(defaultBack, this));
-            }
-        };
-        this.showHome = function() {
-            $('#back').hide();
-            $('#home').show();
-        };
-        this.showView = function(view, options) {
-            // Options should be a js object with optional arguments
-            // options.el -- is the element to render the view within (defaults to #content)
-            // options.back -- boolean which when set to true will display the back button
-            options = options ? options : {};
-            var content = options.el ? options.el : $("#content");
-            if (this.currentView){
-                this.currentView.remove();
-                this.currentView.unbind();
-                if (this.currentView.onClose) {
-                    this.currentView.onClose();
+                if (this.layouts[name]) {
+                    var RequestedLayout = this.layouts[name];
+                    this.currentLayout = new RequestedLayout();
+                    //
+                    // Attach to DOM and render
+                    $('#content').empty().append(this.currentLayout.el);
+                    this.currentLayout.render();
+                    return this.currentLayout;
+                } else {
+                    throw "Layout doesn't exist";
                 }
             }
-            if (options.back && (this.pageStack > 0)) {
-                // there are a couple of edge cases here
-                // the core problem is registering the back click event multiple times to prevent this:
-                //  * Remove any existing click handlers before adding one
-                //  * When the back button is clicked it removes its own event handler
-                //    - This covers the case where we have other 'smarter' back buttons in existence
-                //
-                //  The pageStack is a simple count of how many pages have been loaded for each page loaded
-                //  we add 1 to pageStack, when the user clicks our back button we reduce pageStack by 2
-                //  Effectively resetting the pageStack (you have to remove 2 because when you go back you
-                //  will be calling showView once to render the view you're returning to [we have no idea
-                //  that the user clicked the back button due to history stuff])
-                this.showBack();
-            } else {
-                this.showHome();
-            }
-            this.currentView = view;
-            this.currentView.render();
-            content.html(this.currentView.el);
-            this.pageStack++;
-        };
-    }
-    // This needs to be a global singleton
-    var app = new App();
+        }
+    };
+
     return app;
 });
