9e77425d33f68aae9eada6209a0b871ebd559024
David King <dave@davbo.org>
2013-03-01 18:09:11 +0000

    Generic renderView function for rendering main
    Refactored out of getLayout, allows for rendering views without a layout

--- app/app.js
+++ app/app.js
@@ -5,18 +5,27 @@ define(['jquery', 'underscore', 'core/views/MapBrowseLayout'], function($, _, Ma
 
         layouts: {'MapBrowseLayout': MapBrowseLayout},
 
+        renderView: function(view) {
+            // Remove any existing layouts
+            // If managed with LayoutManager this will call cleanup
+            if (this.currentLayout) {
+                this.currentLayout.remove();
+            }
+            // Attach a view to the DOM and call render
+            this.currentLayout = view;
+            $('#content').empty().append(this.currentLayout.el);
+            this.currentLayout.render();
+        },
+
         getLayout: function(name) {
             if (this.currentLayout && this.currentLayout.name === name) {
                 return this.currentLayout;
             } else {
                 if (this.layouts[name]) {
                     var RequestedLayout = this.layouts[name];
-                    this.currentLayout = new RequestedLayout();
-                    //
-                    // Attach to DOM and render
-                    $('#content').empty().append(this.currentLayout.el);
-                    this.currentLayout.render();
-                    return this.currentLayout;
+                    layout = new RequestedLayout();
+                    this.renderView(layout);
+                    return layout;
                 } else {
                     throw "Layout doesn't exist";
                 }
