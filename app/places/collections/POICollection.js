ee8273a4b3cb3e3dccc052441872944afcf1fbc6
David King <dave@davbo.org>
2013-03-04 15:45:39 +0000

    Facets is no longer a Backbone.Collection

--- app/places/collections/POICollection.js
+++ app/places/collections/POICollection.js
@@ -6,7 +6,6 @@ define(["backbone", "underscore", "places/models/POIModel", "moxie.conf", 'moxie
 
         initialize: function(query) {
             this.query = query || {};
-            this.facets = new Backbone.Collection();
         },
 
         followUser: function() {
@@ -43,7 +42,7 @@ define(["backbone", "underscore", "places/models/POIModel", "moxie.conf", 'moxie
             // Called when we want to empty the existing collection
             // For example when a search is issued and we clear the existing results.
             this.next_results = data._links['hl:next'];
-            this.facets.reset(data._links['hl:types']);
+            this.facets = data._links['hl:types'];
             return data._embedded;
         },
 
