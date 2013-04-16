1466c6737705c35db958321d280f499d491bfb9b
David King <dave@davbo.org>
2013-04-16 19:18:20 +0100

    Comment on creating MapView as it is required

--- app/core/views/MapBrowseLayout.js
+++ app/core/views/MapBrowseLayout.js
@@ -5,6 +5,13 @@ define(['backbone', 'core/views/MapView', 'hbs!core/templates/map-browse'], func
         template: mapBrowseTemplate,
         className: 'map-browse-layout',
         name: 'MapBrowseLayout',
+
+        // Previously we set this view in 'views' this is WRONG
+        //
+        // That way the view is a class attribute rather than being
+        // created when it is required, as it is here (beforeRender)
+        //
+        // See commit #6511cae
         beforeRender: function() {
             this.setView(".content-map", new MapView());
         }
