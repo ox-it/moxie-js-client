adf320ef37e64c73960ffd575b0245a49e891f38
David King <dave@davbo.org>
2013-04-18 13:41:50 +0100

    Contacts routes
    TODO: Move to contacts/router.js

--- app/router.js
+++ app/router.js
@@ -1,9 +1,11 @@
-define(["app", "backbone", "places/router", "today/views/IndexView", "courses/router", "library/router", "favourites/views/FavouritesView"], function(app, Backbone, PlacesRouter, IndexView, CoursesRouter, LibraryRouter, FavouritesView){
+define(["app", "underscore", "backbone", "places/router", "today/views/IndexView", "courses/router", "library/router", "contacts/collections/ContactCollection", "contacts/views/SearchView", "favourites/views/FavouritesView"], function(app, _, Backbone, PlacesRouter, IndexView, CoursesRouter, LibraryRouter, Contacts, ContactsView, FavouritesView){
     var MoxieRouter = Backbone.Router.extend({
+        contactsCollection: new Contacts(),
         subrouters: {},
         routes: {
             "": "index",
             "favourites/": "favourites",
+            "contacts*": "contacts",
 
             "places/*subroute": "placesModule",
             "courses/*subroute": "coursesModule",
@@ -16,6 +18,15 @@ define(["app", "backbone", "places/router", "today/views/IndexView", "courses/ro
 
         favourites: function(params) {
             app.renderView(new FavouritesView({params: params}), {menu: true});
+        },
+
+        contacts: function(params) {
+            var query = params || {};
+            if (!(_.isEqual(query, this.contactsCollection.query) && (this.contactsCollection.length))) {
+                this.contactsCollection.query = query;
+                this.contactsCollection.fetch();
+            }
+            app.renderView(new ContactsView({collection: this.contactsCollection, params: params}), {menu: true});
         },
 
         placesModule: function(subroute) {
