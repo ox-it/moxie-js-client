c046f0bc364f2910e50f0aedfd883e48e9b3e58c
David King <dave@davbo.org>
2013-04-25 15:39:38 +0100

    Entries now stored in a collection on the Feed

--- app/news/models/FeedModel.js
+++ app/news/models/FeedModel.js
@@ -1,15 +1,15 @@
-define(["backbone", "underscore", "goog!feeds,1"], function(Backbone, _) {
+define(["backbone", "underscore", "news/collections/EntryCollection", "goog!feeds,1"], function(Backbone, _, Entries) {
 
     var Feed = Backbone.Model.extend({
         initialize: function() {
+            this.entries = new Entries();
             var feed = new google.feeds.Feed(this.get('url'));
             feed.setNumEntries(10);
             feed.load(_.bind(this.loaded, this));
         },
         loaded: function(result) {
             if (!result.error) {
-                console.log(result.feed.entries);
-                this.set({entries: result.feed.entries});
+                this.entries.reset(result.feed.entries, {parse: true});
             } else {
                 console.log("Failed to load RSS Feed");
             }
