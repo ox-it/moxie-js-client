2034517194964cc09dd80c68c29cccf72fe71195
David King <dave@davbo.org>
2013-04-25 17:29:47 +0100

    Added a core collection MoxieCollection
    Currently this implements "getAsync" a method to provide an async API to
    the existing Collection.get. This way we can call get on a Model pending
    it being loaded into the collection.

---
+++ app/core/collections/MoxieCollection.js
@@ -0,0 +1,17 @@
+define(['backbone', 'underscore'], function(Backbone, _) {
+    var MoxieCollection = Backbone.Collection.extend({
+        getAsync: function(id, options, retry) {
+            options = options || {};
+            var m = this.get(id);
+            if (!m && this.length===0 && !retry) {
+                var pendingEvent = options.pendingEvent || 'reset';
+                this.once(pendingEvent, _.bind(this.getAsync, this, id, options, true));
+            } else if (!m && options.failure) {
+                options.failure();
+            } else if (m && options.success) {
+                options.success(m);
+            }
+        }
+    });
+    return MoxieCollection;
+});
