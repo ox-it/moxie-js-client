caa5553a8d37908ad4eff661e1f7ab304328cfac
David King <dave@davbo.org>
2013-04-26 12:21:33 +0100

    Specify context

--- app/core/collections/MoxieCollection.js
+++ app/core/collections/MoxieCollection.js
@@ -5,7 +5,7 @@ define(['backbone', 'underscore'], function(Backbone, _) {
             var m = this.get(id);
             if (!m && this.length===0 && !retry) {
                 var pendingEvent = options.pendingEvent || 'reset';
-                this.once(pendingEvent, _.bind(this.getAsync, this, id, options, true));
+                this.once(pendingEvent, _.bind(this.getAsync, this, id, options, true), this);
             } else if (!m && options.failure) {
                 options.failure();
             } else if (m && options.success) {
