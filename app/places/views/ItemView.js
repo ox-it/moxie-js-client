42e139b8b660721e383751d1b2ca1474d7fe59e2
David King <dave@davbo.org>
2013-05-13 18:47:20 +0100

    Listen for the POI to be highligted and scroll
    Toggles the class and scroll to the POI

--- app/places/views/ItemView.js
+++ app/places/views/ItemView.js
@@ -1,5 +1,23 @@
-define(["backbone", "hbs!places/templates/item"], function(Backbone, itemTemplate){
+define(["backbone", "underscore", "hbs!places/templates/item"], function(Backbone, _, itemTemplate){
     var ItemView = Backbone.View.extend({
+        initialize: function() {
+            this.model.on('change:highlighted', _.bind(this.highlight, this));
+        },
+        highlight: function(poi) {
+            var scrollEl = $('.content-browse');
+            // Test if we have a scrollable div (ack.)
+            if (scrollEl.get(0).scrollHeight > scrollEl.height()) {
+                scrollEl.scrollTop((scrollEl.scrollTop() + this.$el.position().top) - 70);
+            } else {
+                scrollEl = $(window);
+                scrollEl.scrollTop(this.$el.position().top);
+            }
+            if (poi.get('highlighted')) {
+                this.$el.addClass('highlighted');
+            } else {
+                this.$el.removeClass('highlighted');
+            }
+        },
         manage: true,
         tagName: "li",
         serialize: function() { return this.model.toJSON(); },
