46a42d7228386d3b7d54fe72167758644bd7ab28
David King <dave@davbo.org>
2013-05-13 18:52:08 +0100

    Only scroll to the highlighted poi

--- app/places/views/ItemView.js
+++ app/places/views/ItemView.js
@@ -4,16 +4,21 @@ define(["backbone", "underscore", "hbs!places/templates/item"], function(Backbon
             this.model.on('change:highlighted', _.bind(this.highlight, this));
         },
         highlight: function(poi) {
-            var scrollEl = $('.content-browse');
-            // Test if we have a scrollable div (ack.)
-            if (scrollEl.get(0).scrollHeight > scrollEl.height()) {
-                scrollEl.scrollTop((scrollEl.scrollTop() + this.$el.position().top) - 70);
-            } else {
-                scrollEl = $(window);
-                scrollEl.scrollTop(this.$el.position().top);
-            }
             if (poi.get('highlighted')) {
                 this.$el.addClass('highlighted');
+
+                // Scroll to the POI Selected from the Map
+                //
+                // Only scroll to the element which is being highlighted, not the one being unhighlighted.
+                var scrollEl = $('.content-browse');
+                // Test if we have a scrollable div (ack.)
+                // Effectively making this a test to see if we're in responsive mode...
+                if (scrollEl.get(0).scrollHeight > scrollEl.height()) {
+                    scrollEl.scrollTop((scrollEl.scrollTop() + this.$el.position().top) - 70);
+                } else {
+                    scrollEl = $(window);
+                    scrollEl.scrollTop(this.$el.position().top);
+                }
             } else {
                 this.$el.removeClass('highlighted');
             }
