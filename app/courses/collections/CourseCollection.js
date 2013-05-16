414fd1106bc533b4703ab5967aa0b2f254b2040a
David King <dave@davbo.org>
2013-05-16 16:09:20 +0100

    Updated the courses collection with fetch/parse

--- app/courses/collections/CourseCollection.js
+++ app/courses/collections/CourseCollection.js
@@ -1,12 +1,20 @@
-define(["backbone.hal","courses/models/CourseModel"], function(HAL, course) {
+define(["underscore", "core/collections/MoxieCollection", "courses/models/CourseModel", "moxie.conf"], function(_, MoxieCollection, course, conf) {
 
-    var courses = HAL.Collection.extend({
-
-        model: course
+    var courses = MoxieCollection.extend({
+        model: course,
+        url: conf.urlFor('courses_subjects'),
+        fetch: function() {
+            if (this.ongoingFetch || this.length > 0) { return; }
+            this.ongoingFetch = true;
+            MoxieCollection.prototype.fetch.apply(this, arguments);
+        },
+        parse: function(data) {
+            this.ongoingFetch = false;
+            this.subjects = data._links['courses:subject'];
+            return _.omit(data, '_links');
+        },
 
     });
 
-    // Returns the Model class
     return courses;
-
 });
