18d790e4196a935df532cda7770919112839a56b
David King <dave@davbo.org>
2013-05-16 16:00:52 +0100

    Added courses collection to the CoursesRouter
    To be shared between the views reducing requests.

--- app/courses/router.js
+++ app/courses/router.js
@@ -1,40 +1,40 @@
 define(["app", "underscore", "backbone", "courses/views/SearchView", "courses/views/BookingsView", "courses/views/CoursesView", "courses/views/CourseView", "courses/collections/CourseCollection", "backbone.subroute"],
  function(app, _, Backbone, SearchView, BookingsView, CoursesView, CourseView, Courses){
     var CoursesRouter = Backbone.SubRoute.extend({
+        collection: new Courses(),
 
         routes: {
-            "": "search",
+            "": "subjects",
             "bookings": "bookings",
             "detail/:id": "course",
             ":query": "courses"
         },
 
-        search: function(params) {
-            var options = (_.isEmpty(params)) ? {menu: true} : {};
+        subjects: function() {
+            this.collection.fetch();
             app.renderView(new SearchView({
-                collection: new Courses(),
-                params: params
-            }), options);
+                collection: this.collection,
+            }), {menu: true});
         },
 
         bookings: function(params) {
             app.showView(new BookingsView({
                 params: params
-            }), {back: true});
+            }));
         },
 
         courses: function(query, params) {
             app.showView(new CoursesView({
                 params: params,
                 query: query
-            }), {back: true});
+            }));
         },
 
         course: function(id, params) {
             app.showView(new CourseView({
                 params: params,
                 id: id
-            }), {back: true});
+            }));
         }
 
     });
