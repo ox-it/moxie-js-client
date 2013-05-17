0caf2878e0a809cb75af753194e4c13b1ae1cc19
David King <dave@davbo.org>
2013-05-17 14:18:08 +0100

    parse/url for CourseModel
    Allowing Courses to be retrieved individually.

--- app/courses/models/CourseModel.js
+++ app/courses/models/CourseModel.js
@@ -1,12 +1,19 @@
-define(["backbone.hal"], function(HAL) {
+define(["backbone", "underscore", "moxie.conf"], function(Backbone, _, conf) {
 
-    var Course = HAL.Model.extend({
-        // Model Constructor
-        initialize: function() {
+    var Course = Backbone.Model.extend({
+        url: function() {
+            return conf.urlFor('course_id') + this.id;
+        },
+        parse: function(data) {
+            if (data._embedded && data._embedded.presentations) {
+                data.presentations = data._embedded.presentations;
+            } else {
+                data.presentations = [];
+            }
+            return _.omit(data, ['_embedded', '_links']);
         }
     });
 
-    // Returns the Model class
     return Course;
 
 });
