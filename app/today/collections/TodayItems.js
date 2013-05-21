edddbdb34443329c3b0fb5efd2727779baa36ab9
David King <dave@davbo.org>
2013-05-21 14:59:21 +0100

    Load the Weather and RiverStatus

--- app/today/collections/TodayItems.js
+++ app/today/collections/TodayItems.js
@@ -1,9 +1,11 @@
-define(['core/collections/MoxieCollection', 'today/models/OxfordDate'], function(MoxieCollection, OxfordDate) {
+define(['core/collections/MoxieCollection', 'today/models/OxfordDate', 'today/models/Weather', 'today/models/RiverStatus'], function(MoxieCollection, OxfordDate, Weather, RiverStatus) {
     var TodayItems = MoxieCollection.extend({
         fetch: function() {
             if (this.length===0) {
                 this.reset([
                     new OxfordDate(),
+                    new Weather(),
+                    new RiverStatus(),
                 ]);
                 this.each(function(model) {
                     model.fetch();
