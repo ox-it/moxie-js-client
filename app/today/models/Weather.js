0d84dd1309bd62ede46a3fa3c039fe8e9fede031
David King <dave@davbo.org>
2013-05-22 13:22:53 +0100

    Updated weather component with ss-forecast classes

--- app/today/models/Weather.js
+++ app/today/models/Weather.js
@@ -1,7 +1,31 @@
-define(['backbone', 'moxie.conf', 'today/views/WeatherCard'], function(Backbone, conf, WeatherCard) {
+define(['backbone', 'underscore', 'moxie.conf', 'today/views/WeatherCard'], function(Backbone, _, conf, WeatherCard) {
+    var ssCSSClasses = {
+        's': 'ss-sun',
+        'pc': 'ss-partlycloudy',
+        'si': 'ss-partlycloudy',
+        'm': 'ss-fog',
+        'f': 'ss-fog',
+        'gc': 'ss-clouds',
+        'lrs': 'ss-rain',
+        'hr': 'ss-heavyrain',
+    };
     var Weather = Backbone.Model.extend({
         url: conf.urlFor('weather'),
-        View: WeatherCard
+        View: WeatherCard,
+        setSSClass: function(observation) {
+            // TODO: Will anyone notice that we hard-code to cloudy?
+            observation.ss_class = ssCSSClasses[observation.outlook_icon] ? ssCSSClasses[observation.outlook_icon] : 'ss-clouds';
+            return observation;
+        },
+        parse: function(data) {
+            console.log(data);
+            this.setSSClass(data.observation);
+            _.each(data.forecasts, function(forecast) {
+                this.setSSClass(forecast);
+            }, this);
+            console.log(data);
+            return data;
+        }
     });
     return Weather;
 });
