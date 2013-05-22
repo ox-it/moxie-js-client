fd3ad0a9f30369826117ca3fa1aecfebedea96b0
David King <dave@davbo.org>
2013-05-22 17:24:05 +0100

    Updated weather icons for more coverage

--- app/today/models/Weather.js
+++ app/today/models/Weather.js
@@ -1,20 +1,34 @@
 define(['backbone', 'underscore', 'moxie.conf', 'today/views/WeatherCard'], function(Backbone, _, conf, WeatherCard) {
     var ssCSSClasses = {
+        'NA': 'ss-clouds',
+        'ukn': 'ss-clouds',
+        'cs': 'ss-cloudynight',
         's': 'ss-sun',
         'pc': 'ss-partlycloudy',
         'si': 'ss-partlycloudy',
-        'm': 'ss-fog',
+        'm': 'ss-haze',
         'f': 'ss-fog',
         'gc': 'ss-clouds',
         'lrs': 'ss-rain',
+        'd': 'ss-rain',
         'hr': 'ss-heavyrain',
+        'h': 'ss-hail',
+        'lsn': 'ss-rainsnow',
+        'hsn': 'ss-snow',
+        'tst': 'ss-thunderstorm',
     };
     var Weather = Backbone.Model.extend({
         url: conf.urlFor('weather'),
         View: WeatherCard,
         setSSClass: function(observation) {
             // TODO: Will anyone notice that we hard-code to cloudy?
-            observation.ss_class = ssCSSClasses[observation.outlook_icon] ? ssCSSClasses[observation.outlook_icon] : 'ss-clouds';
+            var classes = ['ss-forecast'];
+            if (ssCSSClasses[observation.outlook_icon]) {
+                classes.push(ssCSSClasses[observation.outlook_icon]);
+            } else {
+                classes.push('ss-clouds');
+            }
+            observation.ss_class = classes.join(' ');
             return observation;
         },
         parse: function(data) {
