4280851896627fb1fcfb40cbe63f9bfb8d0ac56c
David King <dave@davbo.org>
2013-06-05 12:08:42 +0100

    Added the last updated time to river status card
    Includes minor styling changes.

--- app/today/models/RiverStatus.js
+++ app/today/models/RiverStatus.js
@@ -1,7 +1,11 @@
-define(['backbone', 'underscore', 'moxie.conf', 'today/views/RiversCard'], function(Backbone, _, conf, RiversCard) {
+define(['backbone', 'underscore', 'moment', 'moxie.conf', 'today/views/RiversCard'], function(Backbone, _, moment, conf, RiversCard) {
     var RiverStatus = Backbone.Model.extend({
         url: conf.urlFor('rivers'),
-        View: RiversCard
+        View: RiversCard,
+        parse: function(data) {
+            data.lastUpdated = moment(data._last_updated, 'YYYY-MM-DD HH:mm:ss').fromNow();
+            return data;
+        },
     });
     return RiverStatus;
 });
