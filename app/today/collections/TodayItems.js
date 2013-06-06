cdeb0a65922d726162924e65ab8acd37775063f2
David King <dave@davbo.org>
2013-06-06 17:46:39 +0100

    Include BusStop card

--- app/today/collections/TodayItems.js
+++ app/today/collections/TodayItems.js
@@ -1,4 +1,4 @@
-define(['core/collections/MoxieCollection', 'today/models/OxfordDate', 'today/models/Weather', 'today/models/Webcam', 'today/models/RiverStatus'], function(MoxieCollection, OxfordDate, Weather, Webcam, RiverStatus) {
+define(['core/collections/MoxieCollection', 'today/models/OxfordDate', 'today/models/Weather', 'today/models/Webcam', 'today/models/RiverStatus', 'today/models/BusStop'], function(MoxieCollection, OxfordDate, Weather, Webcam, RiverStatus, BusStop) {
     var TodayItems = MoxieCollection.extend({
         fetch: function() {
             if (this.length===0) {
@@ -7,6 +7,7 @@ define(['core/collections/MoxieCollection', 'today/models/OxfordDate', 'today/mo
                     new Weather(),
                     new RiverStatus(),
                     new Webcam(),
+                    new BusStop(),
                 ]);
                 this.each(function(model) {
                     model.fetch();
