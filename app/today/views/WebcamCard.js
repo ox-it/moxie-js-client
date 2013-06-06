7cd1acb70945d4836b0b4ceb23b1cae70c1bc36c
David King <dave@davbo.org>
2013-06-06 15:20:00 +0100

    Only show webcam card once image loaded
    Also added a template to give credit and link to the POI for the webcam.

--- app/today/views/WebcamCard.js
+++ app/today/views/WebcamCard.js
@@ -1,16 +1,29 @@
-define(['today/views/CardView', 'moxie.conf'], function(CardView, conf) {
+define(['underscore', 'today/views/CardView', 'moxie.conf', 'hbs!today/templates/webcam'], function(_, CardView, conf, webcamTemplate) {
     var WebcamCard = CardView.extend({
         weight: 60,
         manage: true,
-        attributes: {'class': 'today webcam'},
+        attributes: {
+            'class': 'today webcam',
+            'style': 'display:none;',
+        },
         serialize: function() {
             return this.model.toJSON();
         },
-        template: function(webcam) {
+        template: webcamTemplate,
+        afterRender: function() {
             var img = new Image();
-            img.src = conf.endpoint + webcam._links.self.href;
-            return img;
-        }
+            img.src = conf.endpoint + this.model.get('_links').self.href;
+            img.alt = this.model.get('description');
+            img.onload = _.bind(this.showEl, this);
+            this.insert(this.el, img);
+            if (img.complete) {
+                // Image cached
+                this.showEl();
+            }
+        },
+        showEl: function() {
+            this.el.style.display = 'inline-block';
+        },
     });
     return WebcamCard;
 });
