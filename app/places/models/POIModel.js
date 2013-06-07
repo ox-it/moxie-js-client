a2f68360fe280bfdc78a705969e66cc79725db12
David King <dave@davbo.org>
2013-06-07 14:46:55 +0100

    Render RTI directly from the POI itself
    This logic might be a bit messy but it saves duplicating code into
    templates. Open to other suggestions.

--- app/places/models/POIModel.js
+++ app/places/models/POIModel.js
@@ -1,8 +1,21 @@
-define(["backbone", "moxie.conf"], function(Backbone, conf) {
+define(["backbone", "underscore", "moxie.conf", "places/models/RTIModel", "places/views/RTIView"], function(Backbone, _, conf, RTI, RTIView) {
 
     var POI = Backbone.Model.extend({
         url: function() {
            return conf.urlFor('places_id') + this.id;
+        },
+
+        renderRTI: function(target, timeout) {
+            var attrs = this.getRTI();
+            // TODO: Currently RTI needs to know which "type" of POI
+            // it is representing. We need to revisit this.
+            attrs.type = this.get('type');
+            this.rti = new RTI(attrs);
+            var rtiView = new RTIView({model: this.rti, el: target});
+            this.rti.fetch();
+            if (timeout) {
+                return setInterval(_.bind(this.rti.fetch, this.rti), timeout);
+            }
         },
 
         getRTI: function() {
