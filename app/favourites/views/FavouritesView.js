c484542322a622af610994fca9021bda856e4895
David King <dave@davbo.org>
2013-07-09 16:42:46 +0100

    FavouritesView -> LayoutManager and set EditMode on the fav button

--- app/favourites/views/FavouritesView.js
+++ app/favourites/views/FavouritesView.js
@@ -1,13 +1,26 @@
 define(['jquery', 'backbone', 'underscore', 'favourites/collections/Favourites', 'hbs!favourites/templates/favourites'],
     function($, Backbone, _, Favourites, favouritesTemplate) {
         var FavouritesView = Backbone.View.extend({
-            initialize: function() {
+            initialize: function(options) {
+                options = options || {};
+                this.button = options.button;
                 this.collection.on("remove add", this.render, this);
                 this.collection.on("change", this.render, this);
             },
-            render: function() {
+            manage: true,
+            template: favouritesTemplate,
+            serialize: function() {
+                return {favourites: this.collection.toJSON()};
+            },
+            afterRender: function() {
                 Backbone.trigger('domchange:title', "Favourites");
-                this.$el.html(favouritesTemplate({favourites: this.collection.toJSON()}));
+                this.button.editMode(_.bind(this.editCallback, this), _.bind(this.saveCallback, this));
+            },
+            editCallback: function() {
+                console.log("Edit callback");
+            },
+            saveCallback: function() {
+                console.log("Save callback");
             }
         });
         return FavouritesView;
