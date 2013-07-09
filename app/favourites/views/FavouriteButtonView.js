f0dadd633e208e39a6895db448c1919ee5a12d65
David King <dave@davbo.org>
2013-07-09 16:43:17 +0100

    Added EditMode to the fav button with callbacks
    Sets the fav button to allow edits to names of favourites and deletions.
    Enabled when on the FavouritesView
    
    Needs cleaning up

--- app/favourites/views/FavouriteButtonView.js
+++ app/favourites/views/FavouriteButtonView.js
@@ -5,11 +5,6 @@ define(['jquery', 'backbone', 'underscore', 'moxie.conf', 'favourites/collection
                 this.collection.on("reset remove add", this.updateButton, this);
                 $(window).on("hashchange", _.bind(this.updateButton, this));
             },
-
-            attributes: {
-                'class': 'generic free-text'
-            },
-
             events: {'click': 'toggleFavourite'},
 
             toggleFavourite: function(e) {
@@ -21,17 +16,63 @@ define(['jquery', 'backbone', 'underscore', 'moxie.conf', 'favourites/collection
                     this.addFavourite();
                 }
             },
+
+            editing: false,
+            toggleEdit: function(e) {
+                e.preventDefault();
+                this.editing = !this.editing;
+                this.setEditState(this.editing);
+                return false;
+            },
+            setEditState: function(editState) {
+                if (editState) {
+                    this.$el.removeClass('ss-lock');
+                    this.$el.addClass('ss-unlock');
+                    this.editCB();
+                } else {
+                    this.$el.removeClass('ss-unlock');
+                    this.$el.addClass('ss-lock');
+                    this.saveCB();
+                }
+            },
+            inEditMode: false,
+            editMode: function(editCB, saveCB) {
+                this.editCB = editCB;
+                this.saveCB = saveCB;
+                this.$el.removeClass('favourited');
+                this.$el.removeClass('ss-star');
+                this.editing = false;
+                this.undelegateEvents();
+                this.events = {'click': 'toggleEdit'};
+                this.delegateEvents();
+                this.setEditState(false);
+                this.inEditMode = true;
+            },
+            exitEditMode: function() {
+                this.$el.removeClass('ss-unlock');
+                this.$el.removeClass('ss-lock');
+                this.$el.addClass('ss-star');
+                this.undelegateEvents();
+                this.events = {'click': 'toggleFavourite'};
+                this.delegateEvents();
+            },
             addFavourite: function() {
                 var fragment = Backbone.history.getFragment(undefined, undefined, true);
                 var params = Backbone.history.getQueryParameters();
                 var title = document.title.split(conf.titlePrefix, 2)[1];
                 var path = Backbone.history.getFragment();
-                this.collection.create({path: path, fragment: fragment, params: params, title: title});
+                this.collection.create({path: path, fragment: fragment, params: params, title: title,
+                    success: function(model) {
+                        Backbone.trigger('favourited', model);
+                    }
+                });
             },
             removeFavourite: function(favourite) {
                 this.collection.remove(favourite);
             },
             updateButton: function() {
+                if (this.inEditMode) { this.exitEditMode(); }
+                this.$el.addClass('ss-star');
                 if (this.collection.getCurrentPage()) {
                     this.$el.addClass('favourited');
                 } else {
