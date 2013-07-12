af7ce79ed93839228230bbb26ee09ed973e5f1f1
David King <dave@davbo.org>
2013-07-12 17:08:30 +0100

    Updated FavouritesView to handle editing and saving Favourites

--- app/favourites/views/FavouritesView.js
+++ app/favourites/views/FavouritesView.js
@@ -4,23 +4,36 @@ define(['jquery', 'backbone', 'underscore', 'favourites/collections/Favourites',
             initialize: function(options) {
                 options = options || {};
                 this.button = options.button;
+                this.button.on('toggleEdit', _.bind(this.editCallback, this));
                 this.collection.on("remove add", this.render, this);
                 this.collection.on("change", this.render, this);
             },
             manage: true,
+            editing: false,
             template: favouritesTemplate,
             serialize: function() {
-                return {favourites: this.collection.toJSON()};
+                return {favourites: this.collection.toJSON(), editing: this.editing};
             },
             afterRender: function() {
                 Backbone.trigger('domchange:title', "Favourites");
-                this.button.editMode(_.bind(this.editCallback, this), _.bind(this.saveCallback, this));
+                this.button.editMode();
             },
-            editCallback: function() {
-                console.log("Edit callback");
+            editCallback: function(editing) {
+                // Test its actually false, not something falsy
+                if (editing===false) {
+                    // Save the changes
+                    var favouriteInput = this.$('input');
+                    _.each(favouriteInput, function(fav) {
+                        var favourite = this.collection.get(fav.id);
+                        favourite.set('userTitle', fav.value);
+                        favourite.save();
+                    }, this);
+                }
+                this.editing = editing;
+                this.render();
             },
-            saveCallback: function() {
-                console.log("Save callback");
+            cleanup: function() {
+                this.button.exitEditMode();
             }
         });
         return FavouritesView;
