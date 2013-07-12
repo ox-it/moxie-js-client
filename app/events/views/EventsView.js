fe4c09fdd0aa7c47cb588c24dd6e060af9296cd5
Martin Filliau <martin@filliau.com>
2013-07-12 17:34:36 +0100

    Added message "no events" in the list view

--- app/events/views/EventsView.js
+++ app/events/views/EventsView.js
@@ -16,12 +16,16 @@ define(['jquery', 'backbone', 'underscore', 'hbs!events/templates/events', 'even
             Backbone.trigger('domchange:title', "Events");
             var todayEvents = [];
             var weekEvents = [];
+            this.eventsToday = false;
+            this.eventsWeek = false;
 
             function addToday(event) {
                 todayEvents.push(new EventListItemView({model: event}));
+                this.eventsToday = true;
             }
             function addWeek(event) {
                 weekEvents.push(new EventListItemView({model: event}));
+                this.eventsWeek = true;
             }
 
             var today = this.collection.where({happeningToday: true});
@@ -33,6 +37,11 @@ define(['jquery', 'backbone', 'underscore', 'hbs!events/templates/events', 'even
                 'ul#today': todayEvents,
                 'ul#week': weekEvents
             });
+        },
+
+        serialize: function() {
+            return {eventsToday: this.eventsToday,
+                    eventsWeek: this.eventsWeek};
         },
 
         cleanup: function() {
