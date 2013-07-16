dd1c1b20926a78ee06fa4c17b48acbb6895f0014
Martin Filliau <martin@filliau.com>
2013-07-16 11:40:42 +0100

    Removed timeFormat helper and moved logic to model

--- app/today/models/Events.js
+++ app/today/models/Events.js
@@ -1,6 +1,16 @@
 define(['backbone', 'moxie.conf', 'moment', 'today/views/EventsCard'], function(Backbone, conf, moment, EventsCard) {
     var today = moment(new Date());
-
+    var DEFAULT_FORMAT = "h:mma";
+    function dateFormat(date) {
+        try {
+            return date.format(DEFAULT_FORMAT);
+        } catch (err) {
+            if ('console' in window) {
+                console.log(err);
+            }
+            return "";
+        }
+    }
     var Events = Backbone.Model.extend({
         url: conf.urlFor('events_list'),
         View: EventsCard,
@@ -11,6 +21,7 @@ define(['backbone', 'moxie.conf', 'moment', 'today/views/EventsCard'], function(
                 var event = events[e];
                 var starts = moment(event.start_time);
                 if (starts.isSame(today, "day")) {
+                    event.formatted_start_time = dateFormat(starts);
                     evToday.push(event);
                 }
             }
