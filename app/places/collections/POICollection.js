2c1aa1e4bbd126b116ae37dea459987399f1caeb
David King <dave@davbo.org>
2013-07-17 17:39:23 +0100

    Updated POICollection and getMap to use getCurrentLocation
    These were the only places where defaultLocation was being accessed
    directly.

--- app/places/collections/POICollection.js
+++ app/places/collections/POICollection.js
@@ -1,10 +1,6 @@
 define(["core/collections/MoxieCollection", "underscore", "places/models/POIModel", "moxie.conf", 'moxie.position'], function(MoxieCollection, _, POI, conf, userPosition) {
 
-    // Limit how often we search when the users geo position updates
-    // Default to 2 seconds
-    var GEOSEARCH_FREQ_LIMIT = 2000;
     var POIs = MoxieCollection.extend({
-
         model: POI,
 
         initialize: function(query) {
@@ -19,25 +15,21 @@ define(["core/collections/MoxieCollection", "underscore", "places/models/POIMode
             userPosition.unfollow(this.handle_geolocation_query, this);
         },
 
-        userLatLon: null,
+        latestUserPosition: null,
         geoFetch: function(options) {
             // Set a boolean for while the fetch is inflight
             this.ongoingFetch = true;
             options = options || {};
-            if (this.userLatLon) {
-                options.headers = options.headers || {};
-                options.headers['Geo-Position'] = this.userLatLon.join(';');
-            }
+            options.headers = options.headers || {};
+            var position = this.latestUserPosition || userPosition.getCurrentLocation();
+            position = [position.coords.latitude, position.coords.longitude];
+            options.headers['Geo-Position'] = position.join(';');
             return this.fetch(options);
         },
-        recentlySearched: false,
+
         handle_geolocation_query: function(position) {
-            this.userLatLon = [position.coords.latitude, position.coords.longitude];
-            if (!this.recentlySearched) {
-                this.geoFetch();
-                this.recentlySearched = true;
-                window.setTimeout(_.bind(function() {this.recentlySearched = false;}, this), GEOSEARCH_FREQ_LIMIT);
-            }
+            this.latestUserPosition = position;
+            this.geoFetch();
         },
 
         fetchNextPage: function() {
