676a9962b1d49eb74678517f6010d52e35ece04a
David King <dave@davbo.org>
2013-07-18 15:10:52 +0100

    Fixed bug where empty queries on contact search would show old results

--- app/contacts/router.js
+++ app/contacts/router.js
@@ -4,8 +4,10 @@ define(["app", "underscore", "backbone", "contacts/collections/ContactCollection
         routes: {'search*': 'search'},
         search: function(params) {
             var query = params || {};
-            if (!(_.isEqual(query, this.contactsCollection.query) && (this.contactsCollection.length))) {
-                this.contactsCollection.query = query;
+            this.contactsCollection.query = query;
+            if (_.isEmpty(query)) {
+                this.contactsCollection.reset([]);
+            } else if (!(_.isEqual(query, this.contactsCollection.query) && (this.contactsCollection.length))) {
                 this.contactsCollection.fetch();
             }
             app.renderView(new ContactsView({collection: this.contactsCollection, params: params}), {menu: true});
