d019fb296b39eefef1938b0972faafd4229baad0
Martin Filliau <martin@filliau.com>
2013-07-24 17:01:09 +0100

    Park and Rides card: attempt to make it nicer when RTI is not available (TRELLO#274More… )

--- app/today/views/ParkAndRidesCard.js
+++ app/today/views/ParkAndRidesCard.js
@@ -12,24 +12,32 @@ define(['today/views/CardView', 'hbs!today/templates/park_and_rides'], function(
             var services = this.model.get('park_and_rides');
             for(var pr in services) {
                 var park = services[pr];
-                var label = park.spaces + " available";
-                var colors = ["#a9d70b", "#f9c802", "#ff0000"];
                 if (park.unavailable) {
-                    label = "No information";
-                    colors = ['#0000FF', '#0000FF', '#0000FF'];
+                    new JustGage({
+                        id: "pr-" + park.identifier,
+                        value: 0,
+                        min: 0,
+                        max: 100,
+                        title: park.name.split("Park")[0].trim(),
+                        label: "NO INFORMATION",
+                        hideValue: true,
+                        hideMinMax: true,
+                        counter: false
+                    });
+                } else {
+                    new JustGage({
+                        id: "pr-" + park.identifier,
+                        value: park.percentage,
+                        min: 0,
+                        max: 100,
+                        title: park.name.split("Park")[0].trim(),
+                        label: park.spaces + " available",
+                        hideValue: true,
+                        hideMinMax: true,
+                        counter: false,
+                        levelColors: ["#a9d70b", "#f9c802", "#ff0000"]
+                    });
                 }
-                new JustGage({
-                    id: "pr-" + park.identifier,
-                    value: park.percentage,
-                    min: 0,
-                    max: 100,
-                    title: park.name.split("Park")[0].trim(),
-                    label: label,
-                    hideValue: true,
-                    hideMinMax: true,
-                    counter: false,
-                    levelColors: colors
-                });
             }
         }
     });
