17624f151806bcaaca9d7ad8ce79a12adb209838
David King <dave@davbo.org>
2013-07-25 15:06:59 +0100

    Set a dayOfWeek attr for Mo Tu etc, and sort week accordingly

--- app/today/models/Weather.js
+++ app/today/models/Weather.js
@@ -1,4 +1,4 @@
-define(['MoxieModel', 'underscore', 'moxie.conf', 'today/views/WeatherCard'], function(MoxieModel, _, conf, WeatherCard) {
+define(['MoxieModel', 'underscore', 'moxie.conf', 'today/views/WeatherCard', 'moment'], function(MoxieModel, _, conf, WeatherCard, moment) {
     var ssCSSClasses = {
         'NA': 'ss-clouds',
         'ukn': 'ss-clouds',
@@ -39,7 +39,15 @@ define(['MoxieModel', 'underscore', 'moxie.conf', 'today/views/WeatherCard'], fu
             this.setSSClass(data.observation);
             _.each(data.forecasts, function(forecast) {
                 this.setSSClass(forecast);
+                // Set moment as an attribute so we can sort easily afterwards
+                forecast.moment = moment(forecast.observed_date);
+                forecast.dayOfWeek = forecast.moment.format("dd");
             }, this);
+            var sortedForecasts = _.sortBy(data.forecasts, function(forecast) { return forecast.moment.unix(); });
+            // Remove the moment's since they aren't being used elsewhere and seem quite bulky objects
+            // Remembering that model attributes should serialize nicely to JSON
+            _.each(data.forecasts, function(forecast) { delete forecast.moment; });
+            data.forecasts = sortedForecasts;
             return data;
         }
     });
