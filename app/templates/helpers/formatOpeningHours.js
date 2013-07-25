f4eedb48d672900535ad16817d4fd2804de77629
Martin Filliau <martin@filliau.com>
2013-07-25 13:42:36 +0100

    First attempt to better format opening hours and highlight rule used to define if it is open or not

---
+++ app/templates/helpers/formatOpeningHours.js
@@ -0,0 +1,32 @@
+define(["handlebars"], function(Handlebars) {
+    function formatOpeningHours(string) {
+        // format opening hours by splitting on ; and highlighting
+        // the rule used to decide if it is open or not
+        if(!string) {
+            return "";
+        }
+        try {
+            var result = TimeDomain.evaluateInTime(string);
+            var parts = string.split(";");
+            var formatted = '';
+            for (var part in parts) {
+                var p = parts[part];
+                // normalising to compare to what is used by TimeDomain
+                var normalised = replaceAll(replaceAll(p, '-', ' - '), ',', ' , ').trim();
+                if (normalised === result.usedrule) {
+                    formatted += "<strong>" + p + "</strong><br />";
+                } else {
+                    formatted += p + "<br />";
+                }
+            }
+            return formatted;
+        } catch (err) {
+            return string;
+        }
+    }
+    function replaceAll(string, target, replacement) {
+        return string.split(target).join(replacement);
+    }
+    Handlebars.registerHelper('formatOpeningHours', formatOpeningHours);
+    return formatOpeningHours;
+});
