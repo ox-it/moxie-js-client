0506736f4e0b6db7667445f414ec52d081d6c2a9
Dave King <dave@davbo.org>
2013-08-13 14:19:14 +0100

    Set an attr on the model whilst a request is mid-flight
    Done by overriding fetch and listening for sync/error which
    are triggered on success/error respectively

--- app/core/models/MoxieModel.js
+++ app/core/models/MoxieModel.js
@@ -1,14 +1,19 @@
 define(['backbone'], function(Backbone) {
     var MoxieModel = Backbone.Model.extend({
+        registeredFetch: false,
+        // By default the fetch method will set an attr
+        // "midFetch" to true whilst the request to the API
+        // is in flight. This attr is removed once done.
         fetch: function(options) {
-            options = options || {};
-            // Set a default error handler
-            if (!options.error) {
-                options.error = _.bind(function() {
-                    // Pass all the error arguments through
-                    // These are [model, response, options]
-                    this.trigger("errorFetching", arguments);
-                }, this);
+            this.set('midFetch', true);
+            if (!this.registeredFetch) {
+                // Careful to only listen on sync/error once
+                this.registeredFetch = true;
+                this.on('sync error', function(model, response, options) {
+                    // We've either got the model successfully or it has failed
+                    // either way we unset the midFetch attr
+                    model.unset('midFetch');
+                });
             }
             return Backbone.Model.prototype.fetch.apply(this, [options]);
         },
