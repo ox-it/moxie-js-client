2eddd928aae63cd6adf01aea031402e5754e809d
Dave King <dave@davbo.org>
2013-08-13 14:21:26 +0100

    Added general solution to rendering errors
    Base ErrorView which takes a message and renders it to the
    specified el. Can be rendered on model 'error' event.

---
+++ app/core/views/ErrorView.js
@@ -0,0 +1,18 @@
+define(['backbone', 'hbs!core/templates/error'], function(Backbone, errorTemplate) {
+    var ErrorView = Backbone.View.extend({
+        // Simple view for displaying an error message to the user
+        initialize: function(options) {
+            options = options || {};
+            if (options.message) {
+                this.message = options.message;
+            }
+        },
+        message: "Error loading resource.",
+        manage: true,
+        serialize: function() {
+            return {message: this.message};
+        },
+        template: errorTemplate,
+    });
+    return ErrorView;
+});
