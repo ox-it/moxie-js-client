f9e55b6db4b6cfdbc8dac39762c200ec2b19dd22
Martin Filliau <martin@filliau.com>
2013-09-17 17:51:27 +0100

    Added device property for Phonegap

--- app/feedback/views/FeedbackView.js
+++ app/feedback/views/FeedbackView.js
@@ -49,8 +49,28 @@ define(['jquery', 'backbone', 'underscore', 'moxie.conf', 'feedback/models/Feedb
             var feedback = {};
             if (this.$("#input-email").val()) { feedback.email = this.$("#input-email").val(); }
             if (this.$("#input-message").val()) { feedback.message = this.$("#input-message").val(); }
+            feedback.device = this.getDeviceProperties();
             // TODO referer?
             this.model.save(feedback);
+        },
+
+        getDeviceProperties: function() {
+            var device = "";
+            if (window.device) {
+                if (window.device.name) {
+                    device +=  " " + window.device.name;
+                }
+                if (window.device.platform) {
+                    device += " " + window.device.platform;
+                }
+                if (window.device.version) {
+                    device += " " + window.device.version;
+                }
+                if (window.device.phonegap) {
+                    device += " Phonegap: " + window.device.phonegap;
+                }
+            }
+            return device;
         }
     });
 
