155cb2b5a025ea270bd0014a3200927f71b12f32
Martin Filliau <martin@filliau.com>
2013-10-09 11:44:53 +0100

    Small refactoring + using name

--- app/contacts/views/ResultItemView.js
+++ app/contacts/views/ResultItemView.js
@@ -8,40 +8,55 @@ define(["backbone", "underscore", "jquery", "app", "hbs!contacts/templates/resul
         addContact: function(ev) {
             ev.preventDefault();
             if ('contacts' in navigator) {
-                var contactEl = ev.target.parentElement.parentElement;
-                var contactName = $(contactEl).find('.contact-name').text();
-                var contactEmail = $(contactEl).find('.contact-info .contact-email').text();
-                var contactExternalTel = $(contactEl).find('.contact-info .contact-external-tel a').text();
-                var contactInternalTel = $(contactEl).find('.contact-info .contact-internal-tel a').text();
-                var contactUnit = $(contactEl).find('.contact-info .contact-unit').text();
                 var contact = navigator.contacts.create({
-                    displayName: contactName,
+                    displayName: this.model.attributes.name
                 });
-                var email = new ContactField();
-                email.pref = true;
-                email.type = "Work";
-                email.value = contactEmail;
-                contact.emails = [email];
+
                 var name = new ContactName();
-                name.formatted = contactName;
-                var organization = new ContactOrganization();
-                organization.pref = true;
-                organization.type = "Work";
-                organization.name = contactUnit;
-                contact.organizations = [organization];
-                var externalTel = new ContactField();
-                externalTel.pref = true;
-                externalTel.type = "External";
-                externalTel.value = contactExternalTel;
-                var internalTel = new ContactField();
-                internalTel.pref = false;
-                internalTel.type = "Internal";
-                internalTel.value = contactInternalTel;
-                contact.phoneNumbers = [externalTel, internalTel],
-                contact.save();
-                navigator.notification.alert(contactName+" has been added to your address book.", _.bind(this.render, this), "Contact Saved");
+                name.formatted = this.model.attributes.name;
+
+                try {
+                    var parts = this.model.attributes.name.split(',');
+                    name.familyName = parts.shift();
+                    name.givenName = parts.join().trim();
+                } catch(err) {
+                    console.log(err);
+                }
+
+                contact.name = name;
+
+                if (this.model.attributes.unit) {
+                    var organization = new ContactOrganization();
+                    organization.pref = true;
+                    organization.type = "Work";
+                    organization.name = this.model.attributes.unit;
+                    contact.organizations = [organization];
+                }
+
+                if (this.model.attributes.email) {
+                    var email = new ContactField();
+                    email.pref = true;
+                    email.type = "Work";
+                    email.value = this.model.attributes.email;
+                    contact.emails = [email];
+                }
+
+                if (this.model.attributes.external_tel) {
+                    var externalTel = new ContactField();
+                    externalTel.pref = true;
+                    externalTel.type = "External";
+                    externalTel.value = this.model.attributes.external_tel;
+                    contact.phoneNumbers = [externalTel];
+                }
+                contact.save(_.bind(this.onContactSaveSuccess, this), _.bind(this.onContactSaveError, this));
             }
             return false;
+        },
+        onContactSaveSuccess: function() {
+            navigator.notification.alert(this.model.attributes.name + " has been added to your address book.", _.bind(this.render, this), "Contact Saved");
+        },
+        onContactSaveError: function(err) {
+            navigator.notification.alert("An error has occured " + err.code, _.bind(this.render, this), "Error");
         },
         serialize: function() {
             return {
