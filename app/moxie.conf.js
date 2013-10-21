c571750fb92772fc944900c9270917bc26a724ca
Martin Filliau <martin@filliau.com>
2013-10-21 13:04:37 +0100

    Updated feed URL for RSL RT#2265471

--- app/moxie.conf.js
+++ app/moxie.conf.js
@@ -64,7 +64,7 @@ define([], function() {
                     {"title": "Mobile Oxford Team Blog", "url": "http://blog.m.ox.ac.uk/feeds/all.atom.xml", "slug": "mox-blog"},
                     {"title": "Department of Biochemistry", "url": "http://rss.oucs.ox.ac.uk/bioch/news/rss20.xml", "slug": "biochemistry-department"},
                     {"title": "Department of Computer Science", "url": "http://www.cs.ox.ac.uk/feeds/News-All.xml", "slug": "cs-news"},
-                    {"title": "News from the RSL", "url": "http://rss.oucs.ox.ac.uk/oxitems/generatersstwo2.php?channel_name=ulib/science", "slug": "ulib-science"},
+                    {"title": "News from the RSL", "url": "http://www.bodleian.ox.ac.uk/science/news/rss", "slug": "ulib-science"},
                     {"title": "Student News Twitter Feed", "url": "https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=uniofoxfordsi", "slug": "student-news-twitter"},
                     {"title": "Somerville College ROQ Blog", "url": "http://roq.some.ox.ac.uk/?feed=atom", "slug": "some-roq"},
                     {"title": "Oxford University Rowing Club Twitter Feed", "url": "http://twitter.com/statuses/user_timeline/5741492.rss", "slug": "rowing-club"},
