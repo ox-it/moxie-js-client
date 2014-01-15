25f6f7ba3ac32e791a2c3dae1fd11766e776a573
David King <dave@davbo.org>
2014-01-15 17:59:43 +0000

    Updated today view card for Notifications with badge showing updates
    Added a div to show the alert icon also

--- app/today/models/Notifications.js
+++ app/today/models/Notifications.js
@@ -1,12 +1,18 @@
-define(['MoxieModel', 'moxie.conf', 'today/views/NotificationsCard'],
-    function(MoxieModel, conf, NotificationsCard) {
+define(['underscore', 'MoxieModel', 'moxie.conf', 'today/views/NotificationsCard'],
+    function(_, MoxieModel, conf, NotificationsCard) {
         var Notifications = MoxieModel.extend({
             url: conf.urlFor('notifications_list'),
             View: NotificationsCard,
             parse: function(data) {
-                if (data._embedded.alerts.length == 0) {
+                if (data._embedded.alerts.length === 0) {
                     return null;
                 } else {
+                    _.each(data._embedded.alerts, function(a) {
+                        if (a._embedded && a._embedded.followups) {
+                            a.updates = true;
+                            a.multipleUpdates = a._embedded.followups.length > 1;
+                        }
+                    });
                     return {alerts: data._embedded.alerts};
                 }
             }
