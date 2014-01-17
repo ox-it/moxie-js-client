686c93ba2ce04e5dd38eaadbac27e6d8a6711f6e
David King <dave@davbo.org>
2014-01-17 14:49:21 +0000

    Updated to work if the security feed is null/undefined

--- app/security/router.js
+++ app/security/router.js
@@ -3,14 +3,19 @@ define(["app", "moxie.conf", "underscore", "backbone", "security/views/StaticVie
     function(app, conf, _, Backbone, StaticView, indexTemplate, Feeds){
 
         var SecurityRouter = Backbone.SubRoute.extend({
-            feed: new Feeds([conf.security.feed]),
+            feed: new Feeds(conf.security.feed),
             routes: {
                 '': 'index',
             },
 
             index: function() {
-                this.feed.load();
-                var security_feed = this.feed.first();
+                var security_feed;
+                if (this.feed.length === 1) {
+                    this.feed.load();
+                    security_feed = this.feed.first();
+                } else {
+                    security_feed = null;
+                }
                 app.renderView(new StaticView({feed: security_feed, template: indexTemplate}));
             },
         });
--- app/security/views/StaticView.js
+++ app/security/views/StaticView.js
@@ -4,15 +4,21 @@ define(['backbone'], function(Backbone) {
         initialize: function(options) {
             this.template = options.template;
             this.feed = options.feed;
-            this.feed.entries.on("reset", this.render, this);
+            if (this.feed) {
+                this.feed.entries.on("reset", this.render, this);
+            }
         },
 
         serialize: function() {
-            var entries = this.feed.entries.toJSON();
-            if (entries.length > 3) {
-                entries = entries.slice(0, 3);
+            if (this.feed) {
+                var entries = this.feed.entries.toJSON();
+                if (entries.length > 3) {
+                    entries = entries.slice(0, 3);
+                }
+                return {feed: this.feed.toJSON(), entries: entries};
+            } else {
+                return {feed: null};
             }
-            return {feed: this.feed.toJSON(), entries: entries};
         },
 
         beforeRender: function() {
